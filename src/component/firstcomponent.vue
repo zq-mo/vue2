a6bbdaca65aad526eddf5b2e11bd0d927496ad81
赵桥 <bridge@zhaoqiaodeMacBook-Pro.local>
2017-03-31 16:44:20 +0800

    first commit

---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="firstcomponent">
+    <h1>I am the first component.</h1>
+    <a> written by {{ author }} </a>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: "bridge"
+    }
+  }
+}
+</script>
+
+<style>
+</style>    
